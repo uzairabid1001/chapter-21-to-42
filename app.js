c3503ddd18e324f93d6e12828624cd65c79e4bf1
uzair abid <uzairabid1001@gmial.com>
2021-12-12 07:07:27 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,88 @@
+//    Question # 1
+// var a=prompt("First Name")
+// var b=prompt("last Name")
+// var fullname = a+b
+// alert (fullname)
+
+//   Question # 2
+// var c= prompt("Enter Favourite Mobile phone model")
+// var d = c.length
+// alert(d)
+
+//   Question # 3
+// var e = ("pakistani")
+// var f = e.indexOf("n")
+// alert (f)
+ 
+//   Question # 4
+//  var g = ("Hello World")
+//  var h = g.lastIndexOf("l")
+//  alert (h)
+
+//   Question # 5
+// var i = ("pakistani")
+// var j = i.charAt("3")
+//  alert (j)
+
+// Question # 6
+// var k =prompt("First Name")
+// var l =prompt("last Name")
+// var fullname = k.concat(l)
+// alert (fullname)
+
+// Question # 7
+// var m = ("Hyderabad")
+// var n = m.replace("Hyder","Islam")
+// alert (n)
+
+// Question # 8
+// var o =("Ali and Sami are best friends. They play cricket and football together.");
+// var p = o.replace(/and/g,"&",)
+// alert (p)
+
+// Question # 9
+// var q =("472")
+// var r = parseInt(q)
+// console.log(r)
+
+
+// Question # 10
+// var s = prompt("enter")
+// var t = s.toUpperCase()
+// console.log(t)
+
+// Question # 11
+// var w = prompt("enter")
+// var x = w.slice(0,1) ;
+// x =x.toUpperCase();
+// var y = w.slice(1);
+// y = y.toLowerCase();
+// console.log(x+y)
+
+// Question # 12
+// var z = (35.36)
+// var aa = z.toString();
+// console.log(aa)
+
+// Question # 14
+// var arr = ["cake", "apple" ,"pie", "cookie", "chips", "patties"];
+// var ab = prompt("Welcome to abc bakery.What do you want to order sir/ma'am?");
+// alert(arr.indexOf(ab))
+
+// Question # 16
+// var text = "University of Karachi";
+// const myArray = text.split("");
+// console.log(myArray)
+
+// Question # 17
+// var ac = prompt("enter")
+// var ad = ac.charAt(ac.length-1)
+// alert(ad)
+
+// var af ="The quick brown fox jumps over the lazy dog"
+// var ag = af.s
+// alert(ag)
+
+// <?php
+echo substr_count("Hello world. The world is nice","world");
+//
